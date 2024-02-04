fb97423c4921653a4289b8284308dcc32ac249a3
Joe Weir <2770814W@student.gla.ac.uk>
2024-02-04 23:11:15 +0000

    chapter 9 done w testfile

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -97,6 +97,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS':{ 'min_length':6 },
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -129,3 +130,5 @@ STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_URL = '/static/'
 
 MEDIA_URL ='/media/'
+
+LOGIN_URL = 'rango:login'
